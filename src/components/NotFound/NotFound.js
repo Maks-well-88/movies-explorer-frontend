6b556aa52bcd20e53f6b64022bb07d7575e543c4
Maksim Pisartsev <Maxwell88@yandex.ru>
2023-01-27 08:13:18 +0300

    Feat: make the layout of the NotFound component

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,5 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+
 import './NotFound.css';
 
 export const NotFound = () => {
-	return <div>NotFound</div>;
+	const navigate = useNavigate();
+	const handleBackButtonClick = () => navigate(-1);
+
+	return (
+		<section className='NotFound'>
+			<div className='NotFound__info'>
+				<h1 className='NotFound__title'>404</h1>
+				<p className='NotFound__text'>Страница не найдена</p>
+			</div>
+			<button className='NotFound__back-button' type='button' onClick={handleBackButtonClick}>
+				Назад
+			</button>
+		</section>
+	);
 };
