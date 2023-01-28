50dd8042cb89784b2b9a9797fc96e960a47b139f
Maksim Pisartsev <makswells88@gmail.com>
2023-01-28 17:24:18 +0300

    Feat: make the layout of the Profile component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,11 +2,13 @@ import { Link } from 'react-router-dom';
 
 import './Navigation.css';
 import Icon from '../../images/account-icon.svg';
+import BurgerMenu from '../../images/burger-menu.svg';
 import { headerNavigationData } from '../../utils/constants';
 
 export const Navigation = () => {
 	return (
 		<nav className='Navigation'>
+			<img className='Navigation__menu' src={BurgerMenu} alt='Меню' />
 			<ul className='Navigation__list'>
 				{headerNavigationData.map((item, i) => (
 					<li key={i} className='Navigation__item'>
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,7 +5,29 @@ export const Profile = () => {
 	return (
 		<>
 			<Header />
-			<div>Profile</div>
+			<section className='Profile'>
+				<div className='Profile__container'>
+					<h1 className='Profile__greeting'>Привет, Виталий!</h1>
+					<div className='Profile__user-name'>
+						<span className='Profile__user-title'>Имя</span>
+						<span>Виталий</span>
+					</div>
+					<div className='Profile__user-email'>
+						<span className='Profile__user-title' lang='en'>
+							E-mail
+						</span>
+						<span lang='en'>pochta@yandex.ru</span>
+					</div>
+					<div className='Profile__buttons-container'>
+						<button type='button' className='Profile__edit-button'>
+							Редактировать
+						</button>
+						<button type='button' className='Profile__logout-button'>
+							Выйти из аккаунта
+						</button>
+					</div>
+				</div>
+			</section>
 		</>
 	);
 };
