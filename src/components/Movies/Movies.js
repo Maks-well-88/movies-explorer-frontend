3149d176634beb25f909292caa1605bc808688d2
Maksim Pisartsev <makswells88@gmail.com>
2023-02-05 17:51:04 +0300

    Feat: add SearchForm to Movies component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,13 @@
 import { Header } from '../Header/Header';
 import { Footer } from '../Footer/Footer';
+import { SearchForm } from '../SearchForm/SearchForm';
 import './Movies.css';
 
 export const Movies = () => {
 	return (
 		<>
 			<Header />
-			<p>Movies</p>
+			<SearchForm />
 			<Footer />
 		</>
 	);
